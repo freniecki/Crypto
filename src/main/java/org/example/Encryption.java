e91b433ffd68549e9345aa978cddff9ada901c31
247773 <247773@edu.p.lodz.pl>
2024-04-19 15:07:37 +0200

    BitSet instead of byte[] implementation

--- src/main/java/org/example/Encryption.java
+++ src/main/java/org/example/Encryption.java
@@ -1,209 +1,257 @@
 package org.example;
 
-import static java.lang.System.arraycopy;
+import java.util.BitSet;
+import java.util.logging.Logger;
+
+import static java.lang.Character.getNumericValue;
 
 public class Encryption {
 
-    public final byte[][] sMatrix = {
-            {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
-                    0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
-                    4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
-                    15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13},
+    Logger logger = Logger.getLogger(getClass().getName());
+    public final int[][] expansionMatrix = {
+            {32, 1, 2, 3, 4, 5, 4, 5},
+            {6, 7, 8, 9, 8, 9, 10, 11},
+            {12, 13, 12, 13, 14, 15, 16, 17},
+            {16, 17, 18, 19, 20, 21, 20, 21},
+            {22, 23, 24, 25, 24, 25, 26, 27},
+            {28, 29, 28, 29, 30, 31, 32, 1}
+    };
+    public final int[][][] sBoxMatrix = {
+            {{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
+                    {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
+                    {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
+                    {15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}},
 
-            {15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
-                    3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
-                    0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
-                    13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9},
+            {{15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
+                    {3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5},
+                    {0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15},
+                    {13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9}},
 
-            {10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
-                    13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
-                    13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
-                    1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12},
+            {{10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
+                    {13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
+                    {13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7},
+                    {1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12}},
 
-            {7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
-                    13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
-                    10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
-                    3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14},
+            {{7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
+                    {13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
+                    {10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4},
+                    {3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14}},
 
-            {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
-                    14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
-                    4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
-                    11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3},
+            {{2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
+                    {14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6},
+                    {4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14},
+                    {11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3}},
 
-            {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
-                    10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
-                    9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
-                    4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13},
+            {{12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
+                    {10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8},
+                    {9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6},
+                    {4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13}},
 
-            {4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
-                    13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
-                    1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
-                    6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12},
+            {{4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1},
+                    {13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6},
+                    {1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2},
+                    {6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12}},
 
-            {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
-                    1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
-                    7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
-                    2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}
+            {{13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
+                    {1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2},
+                    {7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8},
+                    {2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}}
     };
 
-
-    private final byte[][] pBlockMatrix = {
+    private final int[][] pBlockMatrix = {
             {16, 7, 20, 21, 29, 12, 28, 17},
             {1, 15, 23, 26, 5, 18, 31, 10},
             {2, 8, 24, 14, 32, 27, 3, 9},
             {19, 13, 30, 6, 22, 11, 4, 25}
     };
 
+    BitSet changeToOneDimension(BitSet[] twoDimension, int setCount, int bitsCount) {
+        BitSet oneDimension = new BitSet(setCount * bitsCount);
 
-    byte[] changeToOneDimension(byte[][] twoDimension) {
-        int columnLength = twoDimension.length; // evaluate [x][]
-        int rowLength = twoDimension[0].length; // evaluate [][x]
-
-        byte[] oneDimension = new byte[columnLength * rowLength]; // create 1D to return
-
-        for (int i = 0; i < columnLength; i++) {
-            arraycopy(twoDimension[i], 0, oneDimension, i * rowLength, rowLength);
+        for (int i = 0; i < setCount; i++) {
+            for (int j = 0; j < bitsCount; j++) {
+                oneDimension.set(i * setCount + bitsCount, twoDimension[i].get(j));
+            }
         }
+
         return oneDimension;
     }
 
-    byte[][] changeToTwoDimension(byte[] oneDimension, int col, int row) {
-        byte[][] twoDimension = new byte[col][row];
+    BitSet[] changeToTwoDimension(BitSet oneDimension, int col, int row) {
+        BitSet[] twoDimension = new BitSet[col];
 
         for (int i = 0; i < col; i++) {
-            arraycopy(oneDimension, i * row, twoDimension[i], 0, row);
+            twoDimension[i] = new BitSet(row);
+            for (int j = 0; j < row; j++) {
+                twoDimension[i].set(j, oneDimension.get(i * col + row));
+            }
         }
 
         return twoDimension;
     }
 
-    byte[] xorOperation(byte[] param1, byte[] param2) {
-        int array_size = param1.length;
-        byte[] returnValue = new byte[array_size];
-
-        for (int i = 0; i < 48; i++) {
-            returnValue[i] = (byte) (param1[i] + param2[i]);
-            returnValue[i] %= 2;
-        }
-        return returnValue;
-    }
-
-    byte[] changeToArray(int decimal) {
+    /**
+     * I don't know
+     * @param decimal Integer value to change in 4 bits string
+     * @return 4 bits integer representation
+     */
+    BitSet decimalToBitSet(int decimal) {
         String binaryString = Integer.toBinaryString(decimal);
 
         while (binaryString.length() < 4) {
             binaryString = "0" + binaryString;
         }
 
-        byte[] sBoxBin = new byte[4];
+        logger.info(binaryString);
 
+        BitSet sBoxBin = new BitSet(4);
         for (int i = 0; i < 4; i++) {
-            sBoxBin[i] = (byte) (binaryString.charAt(i) - '0');
+            if (getNumericValue(binaryString.charAt(i)) == 1) {
+                sBoxBin.set(i);
+            }
         }
 
         return sBoxBin;
     }
 
-    /*-----------------Mangler methods--------------------*/
+    /**
+     * Expands given half-message to 48 bits
+     *
+     * @param rightHalf 32 bits set from right half of message
+     * @return 48 bits expanded set
+     */
+    BitSet expansionPermutation(BitSet rightHalf) {
+        BitSet[] expandedDraft = new BitSet[6];
 
-
-    // todo: reconsider byte[] implementation - what about bits?? - sth off
-
-    byte[] expansionPermutation(byte[] rightHalf) {
-        byte[][] expandedDraft = new byte[8][6];
-
-        // places 1:1 right half of text
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 4; j++) {
-                expandedDraft[i][j + 1] = rightHalf[i * 4 + j];
+        for (int i = 0; i < 6; i++) {
+            expandedDraft[i] = new BitSet(8);
+            for (int j = 0; j < 8; j++) {
+                expandedDraft[i].set(j, rightHalf.get(expansionMatrix[i][j] - 1));
             }
         }
 
-        // fill 1st column with 4th column, but with moved by one numbers
-        for (int i = 0; i < 7; i++) {
-            expandedDraft[i + 1][0] = expandedDraft[i][4];
-        }
-        expandedDraft[0][0] = expandedDraft[7][4];
-
-        // fill 6th column with 2nd column, but with moved by one numbers
-        for (int i = 1; i < 8; i++) {
-            expandedDraft[i - 1][5] = expandedDraft[i][1];
-        }
-        expandedDraft[7][5] = expandedDraft[0][1];
-
-        return changeToOneDimension(expandedDraft);
+        return changeToOneDimension(expandedDraft, 6, 8);
     }
 
-    byte[] sBox(byte[] sBox, byte[] innerXor) {
-        String stringColumn = String.valueOf(sBox[0]) + String.valueOf(sBox[5]);
+    /**
+     * Given string with '1' and '0' are changed to positions in sBox matrix
+     * and return decimal value present in that position
+     *
+     * @param sBoxString 6 bits string
+     * @param sBoxNumber number of matrix to get value from
+     * @return Decimal value from given matrix at counted position
+     */
+    int sBox(String sBoxString, int sBoxNumber) {
+        String stringColumn = sBoxString.charAt(0) + sBoxString.substring(5);
+        logger.info(stringColumn);
+        String stringRow = sBoxString.substring(1, 5);
+        logger.info(stringRow);
+
         int columnIndex = Integer.parseInt(stringColumn, 2);
-
-        String stringRow = String.valueOf(sBox[1]) + String.valueOf(sBox[2])
-                + String.valueOf(sBox[3]) + String.valueOf(sBox[4]);
-
         int rowIndex = Integer.parseInt(stringRow, 2);
 
-//        byte[][] innerXor2D = changeToTwoDimension(innerXor, 4, 8);
-//        return changeToArray(innerXor2D[columnIndex][rowIndex]);
-
-        return changeToArray(innerXor[columnIndex * 4 + rowIndex]);
+        return sBoxMatrix[sBoxNumber][columnIndex][rowIndex];
     }
 
-    byte[] keySubstitution(byte[] innerXor) {
-        // dostaje klucz 48 bitowy -> zwraca 32 bitowy
-
-        byte[] sBoxSix = new byte[8];
-        byte[] sBoxFour;
-        byte[] substituteKey = new byte[32];
+    /**
+     * Given 48 bits xor product of expanded right half and key creates 32 bits word
+     *
+     * @param innerXor 48 bits xor product of right half and key
+     * @return Shortened 32 bits word
+     */
+    BitSet keySubstitution(BitSet innerXor) {
+        BitSet sBoxSix = new BitSet(6);
+        BitSet sBoxFour;
+        BitSet substituteKey = new BitSet(32);
 
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 6; j++) {
-                sBoxSix[j] = innerXor[i * 6 + j];
+                sBoxSix.set(j, innerXor.get(i * 6 + j));
             }
 
-            sBoxFour = sBox(sBoxSix, innerXor);
+            sBoxFour = decimalToBitSet(sBox(bitsToString(sBoxSix, 6), i));
 
             for (int j = 0; j < 4; j++) {
-                substituteKey[i * 4 + j] = sBoxFour[j];
+                substituteKey.set(i * 4 + j, sBoxFour.get(j));
             }
         }
 
         return substituteKey;
     }
 
-    byte[] permutationBox(byte[] substitutedKey) {
-        byte[] permutatedKey = new byte[32];
+    /**
+     * Permutate 32 bit word by permutation matrix
+     * @param substitutedKey 32 bit result of sBoxing
+     * @return 32 bit after round permutation
+     */
+    BitSet permutationFunction(BitSet substitutedKey) {
+        BitSet permutatedKey = new BitSet(32);
 
         for (int i = 0; i < 32; i++) {
             int index = pBlockMatrix[i / 4][i % 4];
-            permutatedKey[i] = substitutedKey[index];
+            permutatedKey.set(i, substitutedKey.get(index));
         }
         return permutatedKey;
     }
 
-    byte[] manglerFunction(byte[] rightHalf, byte[] leftHalf, byte[] roundKey) {
-        byte[] newRightHalf = permutationBox(keySubstitution(xorOperation(expansionPermutation(rightHalf), roundKey)));
+    /**
+     * Process left and right half to get new rightHalf
+     * @param rightHalf 32 bits of right half of message
+     * @param leftHalf 32 bits of left half of message
+     * @param roundKey generated 48 bits from key
+     * @return New rightHalf based on mangler algorithm
+     */
+    BitSet manglerFunction(BitSet rightHalf, BitSet leftHalf, BitSet roundKey) {
+        BitSet expanded = expansionPermutation(rightHalf);
+        expanded.xor(roundKey);
+        BitSet newRightHalf = permutationFunction(keySubstitution(expanded));
 
-        newRightHalf = xorOperation(newRightHalf, leftHalf);
+        newRightHalf.xor(leftHalf);
 
         return newRightHalf;
     }
+    
+    BitSet encryption(BitSet message, BitSet key) {
+        BitSet encryptedMessage = new BitSet(64);
+        
+        BitSet leftHalf = new BitSet(32);
+        BitSet rightHalf = new BitSet(32);
 
+        for (int i = 0; i < 16; i++) {
+            rightHalf = manglerFunction(rightHalf, leftHalf, key);
+            leftHalf = rightHalf;
+        }
+        
+        return encryptedMessage;
+    }
 
     /*-----------------Key methods--------------------*/
-    byte[] getEffectiveKey(byte[] inputKey) {
-        byte[] effectiveKey = new byte[7];
+    BitSet getEffectiveKey(BitSet inputKey) {
+        BitSet effectiveKey = new BitSet(7);
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 7; j++) {
-                effectiveKey[i * 7 + j] = inputKey[i * 8 + j];
+                effectiveKey.set(i * 7 + j, inputKey.get(i * 8 + j));
             }
         }
         return effectiveKey;
     }
 
-    byte[] getEffectiveKey(String inputKey) {
-        byte[] effectiveKey = new byte[7];
-
-
+    /**
+     * Return clean string of given bits count
+     *
+     * @param bitSet BitSet object
+     * @param length Number of bits to be returned
+     * @return Clean string with '0' and '1'
+     */
+    public static String bitsToString(BitSet bitSet, int length) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            if (bitSet.get(i)) {
+                stringBuilder.append("1");
+            } else {
+                stringBuilder.append("0");
+            }
+        }
+        return stringBuilder.toString();
     }
 }
--- src/test/java/org/example/EncryptionTest.java
+++ src/test/java/org/example/EncryptionTest.java
@@ -3,171 +3,167 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.BitSet;
 
 class EncryptionTest {
 
-    @Test
-    void changeToOneDimension() {
-        Encryption main = new Encryption();
-
-        byte[][] twoDim = {
-                {1, 2, 3, 4, 5, 6, 7, 8},
-                {9, 10, 11, 12, 13, 14, 15, 16},
-                {17, 18, 19, 20, 21, 22, 23, 24},
-                {25, 26, 27, 28, 29, 30, 31, 32}
-        };
-
-        byte[] oneDim = {
-                1, 2, 3, 4, 5, 6, 7, 8,
-                9, 10, 11, 12, 13, 14, 15, 16,
-                17, 18, 19, 20, 21, 22, 23, 24,
-                25, 26, 27, 28, 29, 30, 31, 32
-        };
-
-        Assertions.assertArrayEquals(oneDim, main.changeToOneDimension(twoDim));
-    }
-
-    @Test
-    void changeToTwoDimension() {
-        Encryption main = new Encryption();
-
-        byte[] oneDim = {
-                1, 2, 3, 4, 5, 6, 7, 8,
-                9, 10, 11, 12, 13, 14, 15, 16,
-                17, 18, 19, 20, 21, 22, 23, 24,
-                25, 26, 27, 28, 29, 30, 31, 32
-        };
-
-        byte[][] twoDim = {
-                {1, 2, 3, 4, 5, 6, 7, 8},
-                {9, 10, 11, 12, 13, 14, 15, 16},
-                {17, 18, 19, 20, 21, 22, 23, 24},
-                {25, 26, 27, 28, 29, 30, 31, 32}
-        };
-
-        Assertions.assertArrayEquals(twoDim, main.changeToTwoDimension(oneDim, 4, 8));
-    }
-
-    @Test
-    void expansionPermutation() {
-        Encryption main = new Encryption();
-
-        byte[] rightHalf = {
-                1, 2, 3, 4, 5, 6, 7, 8,
-                9, 10, 11, 12, 13, 14, 15, 16,
-                17, 18, 19, 20, 21, 22, 23, 24,
-                25, 26, 27, 28, 29, 30, 31, 32
-        };
-
-        byte[] expanded = {
-                32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8,
-                9, 10, 11, 12, 13, 12, 13, 14, 15, 16,
-                17, 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24,
-                25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1
-        };
-
-        Assertions.assertArrayEquals(expanded, main.expansionPermutation(rightHalf));
-    }
-
-    @Test
-    void expandedXorRoundKey() {
-        Encryption main = new Encryption();
-        byte[] exp = new byte[48];
-        byte[] key = new byte[48];
-        exp[0] = 0;
-        exp[1] = 1;
-        key[0] = 1;
-        key[1] = 1;
-
-        System.out.println(Arrays.toString(main.xorOperation(exp, key)));
-    }
+//    @Test
+//    void changeToOneDimension() {
+//        Encryption main = new Encryption();
+//
+//        byte[][] twoDim = {
+//                {1, 2, 3, 4, 5, 6, 7, 8},
+//                {9, 10, 11, 12, 13, 14, 15, 16},
+//                {17, 18, 19, 20, 21, 22, 23, 24},
+//                {25, 26, 27, 28, 29, 30, 31, 32}
+//        };
+//
+//        byte[] oneDim = {
+//                1, 2, 3, 4, 5, 6, 7, 8,
+//                9, 10, 11, 12, 13, 14, 15, 16,
+//                17, 18, 19, 20, 21, 22, 23, 24,
+//                25, 26, 27, 28, 29, 30, 31, 32
+//        };
+//
+//        Assertions.assertArrayEquals(oneDim, main.changeToOneDimension(twoDim));
+//    }
+//
+//    @Test
+//    void changeToTwoDimension() {
+//        Encryption main = new Encryption();
+//
+//        byte[] oneDim = {
+//                1, 2, 3, 4, 5, 6, 7, 8,
+//                9, 10, 11, 12, 13, 14, 15, 16,
+//                17, 18, 19, 20, 21, 22, 23, 24,
+//                25, 26, 27, 28, 29, 30, 31, 32
+//        };
+//
+//        byte[][] twoDim = {
+//                {1, 2, 3, 4, 5, 6, 7, 8},
+//                {9, 10, 11, 12, 13, 14, 15, 16},
+//                {17, 18, 19, 20, 21, 22, 23, 24},
+//                {25, 26, 27, 28, 29, 30, 31, 32}
+//        };
+//
+//        Assertions.assertArrayEquals(twoDim, main.changeToTwoDimension(oneDim, 4, 8));
+//    }
+//
+//    @Test
+//    void expansionPermutation() {
+//        Encryption main = new Encryption();
+//
+//        byte[] rightHalf = {
+//                1, 2, 3, 4, 5, 6, 7, 8,
+//                9, 10, 11, 12, 13, 14, 15, 16,
+//                17, 18, 19, 20, 21, 22, 23, 24,
+//                25, 26, 27, 28, 29, 30, 31, 32
+//        };
+//
+//        byte[] expanded = {
+//                32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8,
+//                9, 10, 11, 12, 13, 12, 13, 14, 15, 16,
+//                17, 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24,
+//                25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1
+//        };
+//
+//        Assertions.assertArrayEquals(expanded, main.expansionPermutation(rightHalf));
+//    }
+//
+//    @Test
+//    void expandedXorRoundKey() {
+//        Encryption main = new Encryption();
+//        byte[] exp = new byte[48];
+//        byte[] key = new byte[48];
+//        exp[0] = 0;
+//        exp[1] = 1;
+//        key[0] = 1;
+//        key[1] = 1;
+//
+//        //System.out.println(Arrays.toString(main.xorOperation(exp, key)));
+//    }
 
     @Test
     void changeToArray() {
         Encryption main = new Encryption();
-        byte[] expected = {0,1,1,0};
-        byte[] actual = main.changeToArray(6);
+        BitSet expected = new BitSet(4);
 
-        System.out.println(Arrays.toString(expected));
-        System.out.println(Arrays.toString(actual));
+        expected.set(2);
+        expected.set(1);
+        BitSet actual = main.decimalToBitSet(6);
 
-        Assertions.assertArrayEquals(expected, actual);
+        expected.stream().forEach(System.out::println);
+        System.out.println(expected.size());
+        System.out.println(actual.size());
+        System.out.println(actual.length());
     }
 
     @Test
     void sBox() {
         Encryption main = new Encryption();
 
-        byte[] sBox6 = {
-                1, 0, 0, 1, 0, 1
-        };
-
-        byte[] sBox6v2 = {
-                1, 0, 0, 1, 1, 1
-        };
-
-
-        //col - 4 // row - 2
-
-        byte[] innerXor = {
-                1, 2, 3, 4, 5, 6, 7, 8,
-                9, 10, 11, 12, 13, 14, 15, 16,
-                17, 18, 19, 20, 21, 22, 23, 24,
-                25, 26, 7, 5, 29, 30, 31, 32
-        };
-
-        byte[] sBox4 = {
-                1,1,1,1
-        };
-
-        byte[] sBox4v2 = {
-                0,1,1,1
-        };
-
-        Assertions.assertArrayEquals(sBox4, main.sBox(sBox6, innerXor));
-
-        System.out.println(Arrays.toString(sBox4v2));
-        System.out.println(Arrays.toString(main.sBox(sBox6v2, innerXor)));
-
-        Assertions.assertArrayEquals(sBox4v2, main.sBox(sBox6v2, innerXor));
+        String str = "110110";
+        Assertions.assertEquals(14, main.sBox(str, 0));
     }
 
     @Test
-    void keySubstitution() {
+    void decimalToBits() {
         Encryption main = new Encryption();
-
-        byte[] afterXor48bits = {
-                0, 0, 0, 0, 0, 0, 0, 0,
-                1, 1, 1, 1, 1, 1, 1, 1,
-                0, 0, 0, 0, 0, 0, 0, 0,
-                1, 1, 1, 1, 1, 1, 1, 1,
-                0, 0, 0, 0, 0, 0, 0, 0,
-                1, 1, 1, 1, 1, 1, 1, 1
-        };
-
-        byte[] returnKey = {
-                1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0,
-                1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1
-        };
-
-        Assertions.assertArrayEquals(returnKey, main.keySubstitution(afterXor48bits));
+        Assertions.assertEquals("{}", main.decimalToBitSet(0).toString());
+        Assertions.assertEquals("{0, 1, 2, 3}", main.decimalToBitSet(15).toString());
     }
-
+//
+//    @Test
+//    void keySubstitution() {
+//        Encryption main = new Encryption();
+//
+//        byte[] afterXor48bits = {
+//                0, 0, 0, 0, 0, 0, 0, 0,
+//                1, 1, 1, 1, 1, 1, 1, 1,
+//                0, 0, 0, 0, 0, 0, 0, 0,
+//                1, 1, 1, 1, 1, 1, 1, 1,
+//                0, 0, 0, 0, 0, 0, 0, 0,
+//                1, 1, 1, 1, 1, 1, 1, 1
+//        };
+//
+//        byte[] returnKey = {
+//                1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0,
+//                1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1
+//        };
+//
+//        Assertions.assertArrayEquals(returnKey, main.keySubstitution(afterXor48bits));
+//    }
+//
+//    @Test
+//    void permutationBox() {
+//    }
+//
     @Test
-    void permutationBox() {
+    void printBits() {
+        BitSet bitSet = new BitSet(6);
+        bitSet.set(1);
+        bitSet.set(3);
+        bitSet.set(4);
+
+        System.out.println(Encryption.bitsToString(bitSet, 6));
     }
-
-
-    String printBits(byte s) {
-        return String.format("%8s",
-                Integer.toBinaryString(s).replace(' ','0'));
-    }
-
-    @Test
-    void testPrint() {
-        System.out.println(printBits((byte) 3));
-        System.out.println(printBits((byte) 17));
-
-    }
+//
+//    @Test
+//    void testShift() {
+//        Encryption encryption = new Encryption();
+//
+//        byte[] right = {
+//                0,0,17,0
+//        };
+//
+//        byte[] expanded = {
+//                0,0,0,0,0,0
+//        };
+//
+//        int index = 20 / 8;
+//        int shift = 20 % 8;
+//        int i = (right[index] >> shift) & 1;
+//
+//        System.out.println(i);
+//    }
 }
