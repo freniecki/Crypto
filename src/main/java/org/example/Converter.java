f23dea33002ffac676725d2e923f8ce80638b0e9
Daniil Shostsik 248680 <248680@edu.p.lodz.pl>
2024-04-22 02:27:22 +0300

    pojawił się problem z konwertacją z boolean do int

---
+++ src/main/java/org/example/Converter.java
@@ -0,0 +1,64 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
+
+import static java.lang.Boolean.valueOf;
+
+public class Converter {
+    private String clearText;
+    private String key;
+
+    public Converter(String clearText, String key) {
+        this.clearText = clearText;
+        this.key = key;
+    }
+
+    /**
+     * converts clear text and string to bitset
+     */
+
+
+    public List<BitSet> convertMessage() {
+        List<BitSet> output = new ArrayList<>();
+        for (int i = 0; i < clearText.length()-1; i++) {
+            BitSet block = new BitSet();
+            char letter = clearText.charAt(i);
+            for (int j = 0; j < 8; j++) {
+                block.set(i, letter%2);
+                letter /= 2;
+            }
+            if (block.size() == 64) {
+                output.add(block);
+            }
+        }
+        return output;
+    }
+    public List<BitSet> convertKey() {
+        List<BitSet> output = new ArrayList<>();
+        for (int i = 0; i < key.length()-1; i++) {
+            BitSet block = new BitSet();
+            char letter = key.charAt(i);
+            for (int j = 0; j < 8; j++) {
+                block.set(i, letter%2);
+                letter /= 2;
+            }
+            if (block.size() == 64) {
+                output.add(block);
+            }
+        }
+        return output;
+    }
+
+    public String convertToString(List<BitSet> binary) {
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < binary.size(); i++) {
+            for (int j = 0; j < binary.get(i).size(); j++) {
+                out.append(toString((binary.get(i).get(j)) * 2));  //nie konwertuje z bool do int i nie widzę metody konwertacji
+            }
+        }
+        boolean k = true;
+        return out.toString();
+    }
+}
