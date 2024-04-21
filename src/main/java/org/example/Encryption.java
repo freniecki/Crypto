fed9352ca7c68d44ec9b998ecd6c9a99c49e8bdf
247773 <247773@edu.p.lodz.pl>
2024-04-22 00:33:27 +0200

    encyrption made, tobe tested

--- src/main/java/org/example/Encryption.java
+++ src/main/java/org/example/Encryption.java
@@ -92,6 +92,17 @@ public class Encryption {
             {34, 53, 46, 42, 50, 36, 29, 32}
     };
 
+    private final int[][] endingPermutation = {
+            {40, 8, 48, 16, 56, 24, 64, 32},
+            {39, 7, 47, 15, 55, 23, 63, 31},
+            {38, 6, 46, 14, 54, 22, 62, 30},
+            {37, 5, 45, 13, 53, 21, 61, 29},
+            {36, 4, 44, 12, 52, 20, 60, 28},
+            {35, 3, 43, 11, 51, 19, 59, 27},
+            {34, 2, 42, 10, 50, 18, 58, 26},
+            {33, 1, 41, 9, 49, 17, 57, 25}
+    };
+
     /*----------------Feistel's methods-----------------*/
 
     /**
@@ -222,12 +233,30 @@ public class Encryption {
         BitSet leftHalf = new BitSet(32);
         BitSet rightHalf = new BitSet(32);
 
+        for (int i = 0; i < 32; i++) {
+            leftHalf.set(i, message.get(i));
+            rightHalf.set(i, message.get(i + 32));
+        }
+
         BitSet effectiveKey = getEffectiveKey(key);
-        BitSet shiftedKey;
         for (int i = 0; i < 16; i++) {
-            shiftedKey = keyShift(effectiveKey, i);
-            rightHalf = manglerFunction(rightHalf, leftHalf, shiftedKey);
-            leftHalf = rightHalf;
+            effectiveKey = keyShift(effectiveKey, i);
+            BitSet tmp = rightHalf;
+            rightHalf = manglerFunction(rightHalf, leftHalf, effectiveKey);
+            leftHalf = tmp;
+        }
+
+        /* get message after 16th round in one bitset */
+        for (int i = 0; i < 32; i++) {
+            leftHalf.set(i + 32, rightHalf.get(i));
+        }
+
+        // ending permutation
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                encryptedMessage.set(i * 8 + j, leftHalf.get(endingPermutation[i][j] - 1));
+            }
+
         }
 
         return encryptedMessage;
