26d14585f4f093c3b79be061c12c1250a5fb6ee8
Daniil Shostsik 248680 <248680@edu.p.lodz.pl>
2024-05-25 02:38:28 +0300

    the begining of implementing Schnorr signature algorithm. Doesn't work so far, commiting just not to lose

---
+++ src/main/java/org/example/Schnorr.java
@@ -0,0 +1,97 @@
+package org.example;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+/**
+ * That algorithm was implemented according to lecture materials
+ **/
+
+public class Schnorr {
+    /**
+     * Parameters of a public key
+     **/
+    private BigInteger p; // p > 2^512
+    private BigInteger q; // q > 2^140
+    private BigInteger h;
+    private BigInteger v;
+
+
+    /**
+     * Private key
+     **/
+    private BigInteger a;
+
+    private static Random random = new Random();
+    public Schnorr(BigInteger a) {
+
+        if((a.compareTo(BigInteger.ONE)) > 0 && a.compareTo(p.subtract(BigInteger.ONE)) < 0) {
+            this.a = a;
+        }
+        else {
+
+        }
+    }
+
+    public static BigInteger generateQ() { // According to algorithm, q > 2^140
+        BigInteger initializeQ = BigInteger.ZERO;
+        while (initializeQ.compareTo(BigInteger.valueOf(2).pow(140)) > 0) {
+            initializeQ = BigInteger.valueOf(random.nextLong());
+        }
+        return initializeQ;
+    }
+
+    private BigInteger geneateP() {
+        BigInteger initializeP = 0;
+
+        return initializeP;
+    }
+
+    /**
+     * Getters. Just for satisfaction [REMOVE IF UNUSED!]
+     **/
+    public BigInteger getP() {
+        return p;
+    }
+
+    public BigInteger getQ() {
+        return q;
+    }
+
+    public BigInteger getH() {
+        return h;
+    }
+
+    public BigInteger getV() {
+        return v;
+    }
+
+    public BigInteger getA() {
+        return a;
+    }
+
+    /**
+     * Setters [THE SAME SH]
+     **/
+    public void setP(BigInteger p) {
+        this.p = p;
+    }
+
+    public void setQ(BigInteger q) {
+        this.q = q;
+    }
+
+    public void setH(BigInteger h) {
+        this.h = h;
+    }
+
+    public void setV(BigInteger v) {
+        this.v = v;
+    }
+
+    public void setA(BigInteger a) {
+        this.a = a;
+    }
+
+
+}
---
+++ src/test/java/org/example/SchnorrAlgorithmTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+
+public class SchnorrAlgorithmTest {
+    @Test
+    void testQInitializer() {
+        // :)
+    }
+
+
+}
